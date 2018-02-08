175eaf5cd3bff8a53762b4e54e5ae06ee16ef276
gb1609 <gb1609@outlook.com>
2018-02-08 17:33:51 +0100

    Updated move and Created second cube

--- src/particles.cpp
+++ src/particles.cpp
@@ -16,28 +16,27 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
-void setCubes(unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
-void drawCubes(unsigned int, Shader);
 
 // settings
 const unsigned int SCR_WIDTH = 1000;
 const unsigned int SCR_HEIGHT = 1000;
+bool mouseActived = false;
 
 // camera
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 9.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
 bool firstMouse = true;
+bool moved = false;
 float yawCamera = -90.0f;
 float pitchCamera = 0.0f;
 float lastX = 800.0f / 2.0;
 float lastY = 600.0 / 2.0;
 float fov = 45.0f;
-bool mouseActived = false;
 
 // timing
-float deltaTime = 0.0f;
+float deltaTime = 0.0f;	// time between current frame and last frame
 float lastFrame = 0.0f;
 
 int main()
@@ -63,11 +62,8 @@ int main()
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
 
-	// tell GLFW to capture our mouse
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
 
-	// glad: load all OpenGL function pointers
-	// ---------------------------------------
 	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
@@ -75,32 +71,65 @@ int main()
 	}
 
 	Shader particlesShader("src/cameraVS.vs", "src/cameraFS.fs");
+	float verticeLC = 0.6f;
+	float verticeBC = 2.0f;
+	Cube cube(verticeLC);
+	float verticesLC[cube.getDimV()];
+	unsigned int indicesLC[cube.getDimI()];
+	cube.setVertexAndIndices(verticeLC, indicesLC, verticesLC);
 
-	unsigned int VBOcubeBC, VAOcubeBC, EBOcubeBC, VBOcubeLC, VAOcubeLC, EBOcubeLC;
-	setCubes(VBOcubeBC, VAOcubeBC, EBOcubeBC, VBOcubeLC, VAOcubeLC, EBOcubeLC);
+	float verticesBC[cube.getDimV()];
+	unsigned int indicesBC[cube.getDimI()];
+	cube.setVertexAndIndices(verticeBC, indicesBC, verticesBC);
 
-	// render loop
-	// -----------
+	unsigned int VBOlc, VAOlc, EBOlc, VBObc, VAObc, EBObc;
 
-	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
-	// -------------------------------------------------------------------------------------------
+	glGenVertexArrays(1, &VAOlc);
+	glGenBuffers(1, &VBOlc);
+	glGenBuffers(1, &EBOlc);
+	glBindVertexArray(VAOlc);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOlc);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesLC), verticesLC, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOlc);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesLC), indicesLC, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glEnableVertexAttribArray(0);
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	particlesShader.use();
-	// pass projection matrix to shader (as projection matrix rarely changes there's no need to do this per frame)
-	// -----------------------------------------------------------------------------------------------------------
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
 	particlesShader.setMat4("projection", projection);
 
+	glGenVertexArrays(1, &VAObc);
+	glGenBuffers(1, &VBObc);
+	glGenBuffers(1, &EBObc);
+	glBindVertexArray(VAObc);
+	glBindBuffer(GL_ARRAY_BUFFER, VBObc);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesBC), verticesBC, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBObc);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesBC), indicesBC, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glEnableVertexAttribArray(0);
+
 	while (!glfwWindowShouldClose(window))
 	{
+		// per-frame time logic
+		// --------------------
 		float currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
+		// input
+		// -----
 		processInput(window);
 
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		// render
+		// ------
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// activate camerashader
 		particlesShader.use();
 
 		// pass projection matrix to shader (note that in this case it could change every frame)
@@ -112,20 +141,27 @@ int main()
 		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 		particlesShader.setMat4("view", view);
 
-		drawCubes(VAOcubeBC, particlesShader);
-		drawCubes(VAOcubeLC, particlesShader);
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
+		glBindVertexArray(VAOlc);
+		glm::mat4 model;
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+		float angle = 0;
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+		particlesShader.setMat4("model", model);
+		glDrawElements(GL_LINES, 32, GL_UNSIGNED_INT, 0);
+
+		glBindVertexArray(VAObc);
+		glDrawElements(GL_LINES, 32, GL_UNSIGNED_INT, 0);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	// optional: de-allocate all resources once they've outlived their purpose:
 	// ------------------------------------------------------------------------
-	glDeleteVertexArrays(1, &VAOcubeBC);
-	glDeleteBuffers(1, &VBOcubeBC);
-	glDeleteVertexArrays(1, &VAOcubeLC);
-	glDeleteBuffers(1, &VBOcubeLC);
+	glDeleteVertexArrays(1, &VAOlc);
+	glDeleteBuffers(1, &VBOlc);
+	glDeleteVertexArrays(1, &VAObc);
+	glDeleteBuffers(1, &VBObc);
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
@@ -139,33 +175,55 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 void processInput(GLFWwindow *window)
 {
+
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+	{
+		cameraPos = glm::vec3(0.0f, 0.0f, 9.0f);
+		cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+		cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+	}
+
 	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS)
 		mouseActived = true;
 	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2) == GLFW_PRESS)
 		mouseActived = false;
+
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
 	float cameraSpeed = 2.5 * deltaTime;
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
 		cameraPos += cameraSpeed * cameraFront;
+		moved = true;
+	}
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
 		cameraPos -= cameraSpeed * cameraFront;
+		moved = true;
+	}
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
 		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+		moved = true;
+	}
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
 		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+		moved = true;
+	}
 }
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
 	if (mouseActived)
 	{
-		if (firstMouse)
+		if (firstMouse || moved)
 		{
 			lastX = xpos;
 			lastY = ypos;
 			firstMouse = false;
+			moved=false;
 		}
-
 		float xoffset = xpos - lastX;
 		float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
 		lastX = xpos;
@@ -202,59 +260,4 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 		fov = 1.0f;
 	if (fov >= 45.0f)
 		fov = 45.0f;
-}
-void setCubes(unsigned int VBOcubeBC, unsigned int VAOcubeBC, unsigned int EBOcubeBC, unsigned int VBOcubeLC,
-		unsigned int VAOcubeLC, unsigned int EBOcubeLC)
-{
-	glGenVertexArrays(1, &VAOcubeBC);
-	glGenBuffers(1, &VBOcubeBC);
-	glGenBuffers(1, &EBOcubeBC);
-	glGenVertexArrays(1, &VAOcubeLC);
-	glGenBuffers(1, &VBOcubeLC);
-	glGenBuffers(1, &EBOcubeLC);
-	float verticeLC = 0.3f;
-	float vertceBC = 0.8f;
-	Cube lcube(verticeLC);
-	unsigned int indicesLC[lcube.getDimI()];
-	unsigned int indicesBC[lcube.getDimI()];
-	float verticesLC[lcube.getDimV()];
-	float verticesBC[lcube.getDimV()];
-	lcube.setVertexAndIndices(verticeLC, indicesLC, verticesLC);
-	lcube.setVertexAndIndices(vertceBC, indicesBC, verticesBC);
-
-	glBindVertexArray(VAOcubeLC);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBOcubeLC);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesLC), verticesLC, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOcubeLC);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesLC), indicesLC, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
-	glEnableVertexAttribArray(0);
-
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-	glBindVertexArray(VAOcubeBC);
-	glBindBuffer(GL_ARRAY_BUFFER, VBOcubeBC);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesBC), verticesBC, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOcubeBC);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesBC), indicesBC, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-}
-void drawCubes(unsigned int VAOcube, Shader particleShader)
-{
-	particleShader.use();
-	glBindVertexArray(VAOcube);
-	glm::mat4 model;
-	model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
-	float angle = 0;
-	model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-	particleShader.setMat4("model", model);
-	glDrawElements(GL_LINES, 32, GL_UNSIGNED_INT, 0);
 }
